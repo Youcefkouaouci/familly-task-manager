823b6d713409b2c268957815325e892d481f9c06
youcefkouaouci <youcefkouaouci@gmail.com>
2025-05-26 07:07:08 +0200

    config dependances

--- src/Repository/TaskAssignmentRepository.php
+++ src/Repository/TaskAssignmentRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repository;
 
+use App\Entity\Task;
 use App\Entity\TaskAssignment;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,28 +18,123 @@ class TaskAssignmentRepository extends ServiceEntityRepository
         parent::__construct($registry, TaskAssignment::class);
     }
 
-//    /**
-//     * @return TaskAssignment[] Returns an array of TaskAssignment objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
 
-//    public function findOneBySomeField($value): ?TaskAssignment
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+     public function save(TaskAssignment $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(TaskAssignment $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * Find tasks assigned to a child with a specific status
+     */
+    public function findByChildAndStatus(User $child, string $status): array
+    {
+        return $this->createQueryBuilder('ta')
+            ->andWhere('ta.child = :child')
+            ->andWhere('ta.status = :status')
+            ->setParameter('child', $child)
+            ->setParameter('status', $status)
+            ->orderBy('ta.lastStatusChangedAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Find all tasks assigned to a child
+     */
+    public function findByChild(User $child): array
+    {
+        return $this->createQueryBuilder('ta')
+            ->andWhere('ta.child = :child')
+            ->setParameter('child', $child)
+            ->orderBy('ta.task.dueDate', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Find tasks assigned to a child that are in progress (pending or accepted)
+     */
+    public function findInProgressTasksByChild(User $child): array
+    {
+        return $this->createQueryBuilder('ta')
+            ->andWhere('ta.child = :child')
+            ->andWhere('ta.status IN (:statuses)')
+            ->setParameter('child', $child)
+            ->setParameter('statuses', [
+                TaskAssignment::STATUS_PENDING,
+                TaskAssignment::STATUS_ACCEPTED
+            ])
+            ->join('ta.task', 't')
+            ->orderBy('t.isUrgent', 'DESC')
+            ->addOrderBy('t.dueDate', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Find tasks assigned to a child for a specific task
+     */
+    public function findByChildAndTask(User $child, Task $task): ?TaskAssignment
+    {
+        return $this->createQueryBuilder('ta')
+            ->andWhere('ta.child = :child')
+            ->andWhere('ta.task = :task')
+            ->setParameter('child', $child)
+            ->setParameter('task', $task)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * Find completed tasks by child
+     */
+    public function findCompletedByChild(User $child): array
+    {
+        return $this->createQueryBuilder('ta')
+            ->andWhere('ta.child = :child')
+            ->andWhere('ta.status = :status')
+            ->setParameter('child', $child)
+            ->setParameter('status', TaskAssignment::STATUS_COMPLETED)
+            ->orderBy('ta.completedAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+    //    /**
+    //     * @return TaskAssignment[] Returns an array of TaskAssignment objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('t.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?TaskAssignment
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Task;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,6 +15,76 @@ class TaskRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Task::class);
+    }
+
+    
+    public function save(Task $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Task $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * Find tasks by parent for the current week
+     */
+    public function findByParentForCurrentWeek(User $parent): array
+    {
+        $startOfWeek = new \DateTimeImmutable('monday this week');
+        $endOfWeek = new \DateTimeImmutable('sunday this week 23:59:59');
+        
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.parent = :parent')
+            ->andWhere('t.createdAt BETWEEN :start AND :end')
+            ->setParameter('parent', $parent)
+            ->setParameter('start', $startOfWeek)
+            ->setParameter('end', $endOfWeek)
+            ->orderBy('t.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Find tasks by parent for the current month
+     */
+    public function findByParentForCurrentMonth(User $parent): array
+    {
+        $startOfMonth = new \DateTimeImmutable('first day of this month midnight');
+        $endOfMonth = new \DateTimeImmutable('last day of this month 23:59:59');
+        
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.parent = :parent')
+            ->andWhere('t.createdAt BETWEEN :start AND :end')
+            ->setParameter('parent', $parent)
+            ->setParameter('start', $startOfMonth)
+            ->setParameter('end', $endOfMonth)
+            ->orderBy('t.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Find urgent tasks
+     */
+    public function findUrgentTasks(): array
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.isUrgent = :isUrgent')
+            ->setParameter('isUrgent', true)
+            ->orderBy('t.dueDate', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 
     //    /**
